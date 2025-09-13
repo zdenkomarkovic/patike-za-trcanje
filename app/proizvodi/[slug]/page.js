860e67eaac4154b6c2ce8f0516230b4ff6c89b8f
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-09-13 10:37:23 +0200

    .

--- app/proizvodi/[slug]/page.js
+++ app/proizvodi/[slug]/page.js
@@ -64,9 +64,9 @@ export default async function ProductPage({ params }) {
 <ImageGallery    images={product.images} productName={product.name} />
 
           {/* Informacije */}
-          <div className="space-y-6">
+          <div className="space-y-10 text-center">
             <div>
-              <h1 className="text-3xl font-bold text-gray-900 mb-2">
+              <h1 className="text-2xl md:text-5xl text font-bold text-gray-900 mb-6">
                 {product.name}
               </h1>
               {product.category && (
@@ -109,14 +109,30 @@ export default async function ProductPage({ params }) {
 
 
 
-            <div className="flex space-x-4">
-              <Button className="flex-1">
-                Dodaj u korpu
-              </Button>
-              <Button variant="outline" className="flex-1">
-                Dodaj u listu želja
-              </Button>
-            </div>
+<div className="flex flex-col gap-1 text-center ">
+          <a
+            href="tel:+381643824647"
+            className="text-white w-fit mx-auto text-xl md:text-2xl flex items-center gap-2 bg-gradient-to-r from-[#f75760] to-[#954ffd] px-5 py-2 rounded-full transition-all duration-300 hover:scale-105 hover:shadow-lg hover:bg-gradient-to-r hover:from-[#954ffd] hover:to-[#f75760] "
+          >
+            <svg
+              className="w-6 h-6 text-white"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+              />
+            </svg>{" "}
+            +381643824647
+          </a>
+          <p className=" text-base mt-1">
+            Pozovite nas za brzu konsultaciju
+          </p>
+        </div>
 
             {/* Dodatne informacije */}
 
